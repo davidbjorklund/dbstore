225f522f00ef4b81905832830d47e71018f1835e
DavidBjorklund <divad.bjo@gmail.com>
2024-01-05 17:57:00 +0100

    Updates

--- precache-manifest.2d5392dd1e686a4864c80384d943148e.js
+++ precache-manifest.2d5392dd1e686a4864c80384d943148e.js
@@ -1,15 +1,15 @@
 self.__precacheManifest = [
   {
-    "revision": "3632439d56f5e24903fa",
+    "revision": "58a6e04e9067b94396b0",
     "url": "/dbstore/static/css/main.8763cc04.chunk.css"
   },
   {
-    "revision": "3632439d56f5e24903fa",
-    "url": "/dbstore/static/js/main.3632439d.chunk.js"
+    "revision": "58a6e04e9067b94396b0",
+    "url": "/dbstore/static/js/main.58a6e04e.chunk.js"
   },
   {
-    "revision": "f1ec986a6cfcdef8dbf9",
-    "url": "/dbstore/static/js/1.f1ec986a.chunk.js"
+    "revision": "a12d26aa10f569af09be",
+    "url": "/dbstore/static/js/1.a12d26aa.chunk.js"
   },
   {
     "revision": "951ff60a8a8ab889661f",
@@ -572,7 +572,7 @@ self.__precacheManifest = [
     "url": "/dbstore/static/media/womens-watches.8b5e41d9.png"
   },
   {
-    "revision": "9624b2cee3db8f33fe4335a32e935261",
+    "revision": "92bbf622fb96f699fd81e9021226acef",
     "url": "/dbstore/index.html"
   }
 ];
